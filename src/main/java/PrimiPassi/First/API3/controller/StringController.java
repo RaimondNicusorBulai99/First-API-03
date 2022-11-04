487ef9529b717b9efadc95ca101dda2feb2b26c9
RaimondnicusorBulai <Condivisionedatiraimond@gmail.com>
2022-11-04 12:06:23 +0100

    second modified: completed Java clas 'StringController' on yours parameters

--- src/main/java/PrimiPassi/First/API3/controller/StringController.java
+++ src/main/java/PrimiPassi/First/API3/controller/StringController.java
@@ -1,4 +1,18 @@
 package PrimiPassi.First.API3.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class StringController {
+
+    @GetMapping("/strings")
+    public String concat2Str(@RequestParam String strOne, @RequestParam(required = false) String strTwo) {
+        if (strTwo == null) {
+            return strOne;
+        } else {
+            return strOne.concat(strTwo);
+        }
+    }
 }
